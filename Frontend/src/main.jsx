3922f02c9031d885a9fb2b0d213611915ea73662
Himanshu Yadav <hy897433@gmail.com>
2025-09-30 23:32:16 +0530

    third commit

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -26,9 +26,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 );
